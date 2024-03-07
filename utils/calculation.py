79c75ae049c2fa3fc45b65c1e9530d51a5de7fe5
Niu <100760855+NiuHuangxiaozi@users.noreply.github.com>
2024-03-07 19:08:02 +0800

    Add files via upload

---
+++ utils/calculation.py
@@ -0,0 +1,78 @@
+import torch
+import time
+import torch.optim as optim
+from thop import profile
+__all__ = ["calculate_time", "calculate_memmory","calculate_param"]
+
+
+#try to calculate the time
+def calculate_time(model,criterion,device,shape:tuple):
+
+    #create a temporary tensor
+    model = model.to(device)
+    temp_tensor=torch.normal(0, 1, size=shape).to(device)
+    torch.cuda.synchronize()
+    start = time.time()
+    result=model(temp_tensor)
+    loss = criterion(result, result - 1e-4)
+    loss.backward()
+    torch.cuda.synchronize()
+    end = time.time()
+    time_cost=end-start
+    return time_cost
+
+
+
+'''
+这个计算模型占用的实际显存的函数在理论方面还有有完全验证成功，
+还需要进一步实验
+'''
+def calculate_memmory(model,
+                      criterion,
+                      optimizer_choice: str,
+                      shape: tuple,
+                      device):
+    torch.cuda.empty_cache()  # 将所有缓冲区的显存分配去除
+    torch.cuda.synchronize()  # GPU等待所有的GPU事务完成
+    before_memory = torch.cuda.memory_allocated(device)
+
+    temp_tensor = torch.normal(0, 1, size=shape).to(device)  # 创建一个简单的张量
+    model.train()
+    model = model.to(device)
+    try:
+        if optimizer_choice == 'Adam':
+            optimizer = optim.Adam(model.parameters(), lr=0.001, )
+        else:
+            raise ValueError
+    except ValueError:
+        print("In calculation.py calculate_memmory function optimizer_choice is wrong!")
+    optimizer.zero_grad()
+    result = model(temp_tensor)
+    torch.cuda.empty_cache()
+    loss = criterion(result, result - 1e-4)
+    loss.backward()
+    optimizer.step()
+    torch.cuda.synchronize()
+    after_memory = torch.cuda.max_memory_allocated(device)
+    return after_memory - before_memory
+
+def calculate_param(model,shape):
+    dummy_input = torch.normal(0, 1, size=shape)
+    flops, params = profile(model, inputs=(dummy_input,))
+    return flops,params
+
+
+'''
+try:
+    output = model(input)
+except RuntimeError as exception:
+    if "out of memory" in str(exception):
+        print("WARNING: out of memory")
+        if hasattr(torch.cuda, 'empty_cache'):
+            torch.cuda.empty_cache()
+'''
+
+
+
+
+
--- utils/optimus.py
+++ utils/optimus.py
@@ -1,20 +1,21 @@
 
-
-
-#自己写的其他的库
+# 自己写的其他的库
 from utils.sampler import Distributed_Elastic_Sampler
 from help.debug import global_rank_print
-
-#exception
+from utils.calculation import calculate_time, calculate_param
+# exception
 from help.debug import MyException
 
-#官方的库
+# 官方的库
+import copy
 import sys
 import torch
 import time
 import numpy as np
 from torch.utils.data import DataLoader
 from torch.nn.parallel import DistributedDataParallel as DDP
+from typing import List ,Union
+import torch.distributed as dist
 
 '''
 这个文件的作用是实现数据集的合理划分以及模型流水线并行的合理划分
@@ -23,55 +24,99 @@ __all__ = ["OptimusSpeed", ]
 
 
 class OptimusSpeed:
-    def __init__(self)->None:
-        self.grad_portion =None #对于数据并行不平衡的batch的normalize
-        #用来计算模型的运行时间并做一些分析
-        self.start_time=0.0
-        self.end_time=0.0
+    def __init__(self ) ->None:
+        self.grad_portion =None  # 对于数据并行不平衡的batch的normalize
+        # 用来计算模型的运行时间并做一些分析
+        self.start_time = 0.0
+        self.end_time = 0.0
 
     '''
     build_model构建我们需要训练的模型
     '''
-    def model_wrap(self,model,device,model_type:str):
+
+    def model_wrap(self, model, device, model_type: str):
         try:
             if model_type == 'ddp':
                 self.wrapped_model = DDP(model.to(device))
             elif model_type == 'pipe':
                 pass
-            elif model_type=='hybrid':
+            elif model_type == 'hybrid':
                 pass
             else:
-                raise  ValueError
+                raise ValueError
         except ValueError:
             print("Current model parallel type only contain ddp, pipe and hybrid(ddp,pipe)")
             sys.exit(1)
         return self.wrapped_model
 
+    def autobalance(self,
+                    model,
+                    world_size,
+                    criterion,
+                    config_param,
+                    args
+                    ) -> Union[List[int], List[int]]:
+        partition_list = None
+        batchsize_list = None
+        try:
+            if config_param.model_type == 'ddp':
+                dummy_model = copy.deepcopy(model)  # 独立拷贝一份模型用作测试防止修改原来的模型
+                if config_param.unbalanced_strategy == "time":
+
+                    dummy_time=calculate_time(dummy_model,criterion,args.local_rank,config_param.data_dummy_shape)
+                    dummy_time_tensor = torch.tensor(dummy_time).to(args.local_rank)
+                    time_list = [torch.zeros(1, ).to(args.local_rank) for i in range(0, world_size)]
+                    print("time_list",time_list)
+                    print("dummy_time_tensor",dummy_time_tensor)
+                    dist.all_gather(time_list, dummy_time_tensor)
+                    print("time_list",time_list)
+                    time_list = [obj.item()*100 for obj in time_list]
+                    rounded_time_array=np.trunc(time_list)/100
+                    #rounded_time_array = (np.rint(time_list)).astype(int)
+                    print("rounded_time_array",rounded_time_array)
+                    portion = rounded_time_array / np.sum(rounded_time_array)
+                    print("portion",portion)
+                    partition_list = (config_param.total_datasize * portion).astype(int)
+                    batchsize_list = (config_param.batch_size * portion).astype(int)
+                    partition_list[-1]=config_param.total_datasize-np.sum(partition_list[:-1])
+                    batchsize_list[-1]=config_param.batch_size-np.sum(batchsize_list[:-1])
+                    return partition_list.tolist(), batchsize_list.tolist()
+                else:
+                    raise MyException(2, "The auto balanced strategy is only support for time and complexity now.")
+            elif config_param.model_type == 'pipe':
+                pass
+            else:
+                raise MyException(3, "The parallel type only support for ddp ,pipe or hybrid")
+        except MyException as e:
+            print(e)
+            sys.exit(1)
+
     def ddp_unbalanced_dataset_split(
-        self,
-        global_rank,
-        dataset,
-        partition_method:str,   # manual or autobalanced，
-        #manual
-        partition_list,
-        batchsize_list,
-        #autobalanced
-        model,
-        train_loader_pin_memory:bool,
-        train_loader_num_workers:int
+            self,
+            global_rank,
+            dataset,
+            partition_list,
+            batchsize_list,
+            model,
+            criterion,
+            world_size,
+            config_param,
+            args
     ) -> torch.utils.data.DataLoader:
         try:
-            if partition_method == 'manual' or  partition_method == 'autobalanced':
-                if partition_method == 'autobalanced':
-                    'TODO'
-                    print("This is partition mode!")
-                    pass
-                #for debug
-                global_rank_print(0,"My global rank is %d. The partition_list is : " % (0)+str(partition_list))
-                global_rank_print(0,"My global rank is %d. The batchsize_list is : " % (0)+str(batchsize_list))
+            if config_param.partition_method == 'manual' or config_param.partition_method == 'autobalanced':
+                if config_param.partition_method == 'autobalanced':
+                    print("This is autobalanced partition mode!")
+                    partition_list, batchsize_list = self.autobalance(model=model, world_size=world_size,
+                                                                      criterion=criterion, config_param=config_param,
+                                                                      args=args)
+
+                # for debug
+                global_rank_print(0, "The partition_list is : " + str(partition_list))
+                global_rank_print(0, "The batchsize_list is : " + str(batchsize_list))
                 if partition_list is None or batchsize_list is None:
                     raise MyException(0, "partition_list or batchsize_list can't be None in the manual mode!")
-                #这里在计算完batchsize_list之后我们需要计算一下各个梯度的权重比例
+                # 这里在计算完batchsize_list之后我们需要计算一下各个梯度的权重比例
                 self.grad_portion = batchsize_list / np.sum(batchsize_list)
 
                 sampler_dict = \
@@ -85,15 +130,17 @@ class OptimusSpeed:
                                           batch_size=batchsize_list[global_rank],
                                           shuffle=False,  # 这个值必须设置为false，否则会导致多个节点可能都抽到同一个样例的结果
                                           sampler=sampler,
-                                          pin_memory=train_loader_pin_memory,
-                                          num_workers=train_loader_num_workers
+                                          pin_memory=config_param.train_loader_pin_memory,
+                                          num_workers=config_param.train_loader_num_workers
                                           )
                 return train_loader
             else:
-                raise MyException(1,"Your choice of partition_method("+str(partition_method)+") is wrong [neither manual or autobalanced!")
+                raise MyException(1, "Your choice of partition_method(" + str(
+                    config_param.partition_method) + ") is wrong [neither manual or autobalanced!")
         except MyException as e:
             print(e)
             sys.exit(1)
+
     def unbalanced_train(self,
                          iter_index,
                          gradient_accumulate_step,
@@ -102,14 +149,14 @@ class OptimusSpeed:
                          inputs,
                          labels,
                          criterion,
-                         optimizer)->torch.Tensor:
-        #前向传播
+                         optimizer) -> torch.Tensor:
+        # 前向传播
         outputs = wrapped_model(inputs)
-        #计算loss值
+        # 计算loss值
         loss = criterion(outputs, labels)
         (loss * self.grad_portion[global_rank]).backward()
 
-        if (iter_index+ 1) % gradient_accumulate_step == 0:
+        if (iter_index + 1) % gradient_accumulate_step == 0:
             # 4.1 update parameters of net
             optimizer.step()
             # 4.2 reset gradient
@@ -117,20 +164,22 @@ class OptimusSpeed:
         return loss
 
     @staticmethod
-    def get_parameter(model)->dict:
+    def get_parameter(model) -> dict:
         total_num = sum(p.numel() for p in model.parameters())
         trainable_num = sum(p.numel() for p in model.parameters() if p.requires_grad)
         return {'Total': total_num, 'Trainable': trainable_num}
 
-    def time_start(self,local_rank,target_local_rank):
-        if local_rank==target_local_rank:
+    def time_start(self, local_rank, target_local_rank):
+        if local_rank == target_local_rank:
             self.start_time = time.time()
-    def time_end(self,local_rank,target_local_rank):
+
+    def time_end(self, local_rank, target_local_rank):
         if local_rank == target_local_rank:
-            self.end_time=time.time()
-    def calculate_time(self,local_rank,target_local_rank):
+            self.end_time = time.time()
+
+    def calculate_time(self, local_rank, target_local_rank):
         if local_rank == target_local_rank:
-            return self.end_time-self.start_time
+            return self.end_time - self.start_time
 
 
 
