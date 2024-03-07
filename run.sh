23729f9908169c36898d1c5f87fe215541f4d8ee
Niu <100760855+NiuHuangxiaozi@users.noreply.github.com>
2024-03-07 19:06:59 +0800

    Add files via upload

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 export CUDA_DEVICE_ORDER="PCI_BUS_ID"
 export NCCL_IB_DISABLE=1
 export NCCL_DEBUG=info
-torchrun --nnodes=2 --nproc_per_node=1 --node_rank=0 --master_addr=192.168.1.70 --master_port=29500 unbalanced_test.py --json_path='/home/args.json'
+torchrun --nnodes=2 --nproc_per_node=1 --node_rank=0 --master_addr=192.168.1.78 --master_port=29500 unbalanced_by_time.py --json_path='/home/args.json'
