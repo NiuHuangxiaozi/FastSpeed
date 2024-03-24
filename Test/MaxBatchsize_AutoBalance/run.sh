84e79a0efdc888870102c864bce14d0bb8f8f9bb
Niu <100760855+NiuHuangxiaozi@users.noreply.github.com>
2024-03-24 20:04:20 +0800

    Delete Test directory
    ready to put first release

--- Test/MaxBatchsize_AutoBalance/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export CUDA_DEVICE_ORDER="PCI_BUS_ID"
-export NCCL_IB_DISABLE=1
-torchrun --nnodes=1 --nproc_per_node=4 --node_rank=0 --master_addr=127.0.0.1 --master_port=29500  main.py
